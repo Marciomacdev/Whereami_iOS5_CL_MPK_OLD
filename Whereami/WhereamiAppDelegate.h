bec2e0c6d6596b65ad994717dcdd731834af1bc3
Marcio Ferreira <marcioferreirass@Marcios-iMac.local>
2011-07-24 09:34:09 -0300

    Initial Commit

---
+++ Whereami/WhereamiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by Marcio Ferreira on 7/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
